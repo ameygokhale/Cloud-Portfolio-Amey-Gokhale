24643f14251129a43e97696529eb2a783827321e
ameygokhale <83856556+ameygokhale@users.noreply.github.com>
2025-12-11 15:52:45 +0530

    Delete projects/cyber-threat directory

--- projects/cyber-threat/run_pipeline.py
+++
@@ -1,5 +0,0 @@
-# run_pipeline.py - simple runner that executes end-to-end steps
-import os
-os.system("python data_gen.py")
-os.system("python preprocess.py --input data/synthetic_network.csv --outdir processed")
-os.system("python train.py --processed processed --outdir models")
